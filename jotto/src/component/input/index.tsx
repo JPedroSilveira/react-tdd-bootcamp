da734b969771d398c0701cb4b5b9d2dfee0cdb85
Joao Pedro Silveira <jpedross1999@gmail.com>
2021-07-12 10:28:10 -0300

    Feat(Jotto>Input): form render implementation

--- jotto/src/component/input/index.tsx
+++ jotto/src/component/input/index.tsx
@@ -1,6 +1,28 @@
+import React from "react"
+
 const Input: React.FC = () => {
+    const [currentGuess, setCurrentGuess] = React.useState("")
+
+    const handleGuessChange = (changeEvent: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentGuess(changeEvent.currentTarget.value)
+    }
+
     return (
         <div data-test='input'>
+            <form data-test='input-form'>
+                <input
+                    data-test='input-form-word-input'
+                    type='text'
+                    placeholder='Enter guess'
+                    value={currentGuess}
+                    onChange={handleGuessChange}
+                />
+                <button 
+                    data-test='input-form-submit-button'
+                >
+                    Submit 
+                </button>
+            </form>
         </div>
     )
 }
