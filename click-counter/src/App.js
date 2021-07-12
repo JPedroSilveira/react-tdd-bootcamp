788b0d09500e1fcca524214510ed06e4dedae419
Joao Pedro Silveira <jpedross1999@gmail.com>
2021-07-11 23:12:46 -0300

    Feat(ClickCounter): increment and decrement implementation

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [withError, setWithError] = useState(false)
+
+  const handleIncrementCounter = () => {
+    removeError()
+    setCount(count + 1)
+  }
+
+  const handleDecrementCounter = () => {
+    removeError()
+    if (count > 0) {
+      setCount(count - 1)
+    } else {
+      setWithError(true)
+    }
+  }
+
+  const removeError = () => {
+    setWithError(false)
+  }
+
   return (
     <div className="App" data-test="app">
-      <h1 data-test="app-counter"> The counter is current 0</h1>
-      <button data-test="app-increment-button">Increment</button>
+      <h1 data-test="app-counter"> The counter is current <span data-test="app-counter-value">{count}</span></h1>
+      {withError && <p data-test="app-error-message">Cannot decrement to negative values</p>}
+      <button data-test="app-increment-button" onClick={handleIncrementCounter}>Increment</button>
+      <button data-test="app-decrement-button" onClick={handleDecrementCounter}>Decrement</button>
     </div>
   );
 }
--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -6,29 +6,108 @@ Enzyme.configure({
   adapter: new EnzymeAdapter()
 })
 
+const setup = () => shallow(<App/>)
+
+const findByTestAttribute = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
 test('render without error', () => {
-  const wrapper = shallow(<App/>)
-  const app = wrapper.find("[data-test='app']")
+  const wrapper = setup()
+  const app = findByTestAttribute(wrapper, 'app')
   expect(app.length).toBe(1)
 
 })
 
 test('render increment button', () => {
-  const wrapper = shallow(<App/>)
-  const incrementButton = wrapper.find("[data-test='app-increment-button']")
+  const wrapper = setup()
+  const incrementButton = findByTestAttribute(wrapper, 'app-increment-button')
   expect(incrementButton.length).toBe(1)
 })
 
 test('render counter display', () => {
-  const wrapper = shallow(<App/>)
-  const appCounter = wrapper.find("[data-test='app-counter']")
+  const wrapper = setup()
+  const appCounter = findByTestAttribute(wrapper, 'app-counter')
   expect(appCounter.length).toBe(1)
 })
 
 test('counter display starts with zero', () => {
-  
+  const wrapper = setup()
+  const appCounterValue = findByTestAttribute(wrapper, 'app-counter-value')
+  expect(appCounterValue.text()).toBe("0")
 })
 
 test('clicking increment button increments counter display', () => {
+  const wrapper = setup()
 
+  const button = findByTestAttribute(wrapper, 'app-increment-button')
+  button.simulate('click')
+
+  const appCounterValue = findByTestAttribute(wrapper, 'app-counter-value')
+  expect(appCounterValue.text()).toBe("1")
+})
+
+test('render decrement button', () => {
+  const wrapper = setup()
+
+  const button = findByTestAttribute(wrapper, 'app-decrement-button')
+  expect(button.length).toBe(1)
+})
+
+test('clicking decrement button decreases counter display', () => {
+  const wrapper = setup()
+
+  const incrementButton = findByTestAttribute(wrapper, 'app-increment-button')
+  incrementButton.simulate('click')
+
+  const decrementButton = findByTestAttribute(wrapper, 'app-decrement-button')
+  decrementButton.simulate('click')
+
+  const appCounterValue = findByTestAttribute(wrapper, 'app-counter-value')
+  expect(appCounterValue.text()).toBe("0")
+})
+
+test('clicking decrement button with zero value does not decrement display counter', () => {
+  const wrapper = setup()
+
+  const decrementButton = findByTestAttribute(wrapper, 'app-decrement-button')
+  decrementButton.simulate('click')
+
+  const appCounterValue = findByTestAttribute(wrapper, 'app-counter-value')
+  expect(appCounterValue.text()).toBe("0")
+})
+
+test('clicking decrement button with zero value shows error message', () => {
+  const wrapper = setup()
+
+  const decrementButton = findByTestAttribute(wrapper, 'app-decrement-button')
+  decrementButton.simulate('click')
+
+  const errorMessage = findByTestAttribute(wrapper, 'app-error-message')
+  expect(errorMessage.length).toBe(1)
+})
+
+test('decrement non-zero value does not show erro message', () => {
+  const wrapper = setup()
+  
+  const incrementButton = findByTestAttribute(wrapper, 'app-increment-button')
+  incrementButton.simulate('click')
+  incrementButton.simulate('click')
+
+  const decrementButton = findByTestAttribute(wrapper, 'app-decrement-button')
+  decrementButton.simulate('click')
+
+  const errorMessage = findByTestAttribute(wrapper, 'app-error-message')
+  expect(errorMessage.length).toBe(0)
+})
+
+test('remove non-negative error when increment button is clicked', () => {
+  const wrapper = setup()
+  
+  const decrementButton = findByTestAttribute(wrapper, 'app-decrement-button')
+  decrementButton.simulate('click')
+
+  const incrementButton = findByTestAttribute(wrapper, 'app-increment-button')
+  incrementButton.simulate('click')
+
+  const errorMessage = findByTestAttribute(wrapper, 'app-error-message')
+  expect(errorMessage.length).toBe(0)
 })
