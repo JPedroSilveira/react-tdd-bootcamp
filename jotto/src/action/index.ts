00f22225ae002143022e50f9949a9de5fc862b7c
Joao Pedro Silveira <jpedross1999@gmail.com>
2021-07-13 16:07:40 -0300

    Feat(Jotto>CorrectGuess): test

--- jotto/src/action/index.ts
+++ jotto/src/action/index.ts
@@ -1,5 +1,15 @@
 import axios from  'axios'
 
+export const actionTypes = { 
+    CORRECT_GUESS: 'CORRECT_GUESS',
+}
+
+export const correctGuess = () => {
+    return {
+        type: actionTypes.CORRECT_GUESS
+    }
+}
+
 export const getSecretWord = async (): Promise<string> => {
     const response = await axios.get('http://localhost:3030')
     return response.data
